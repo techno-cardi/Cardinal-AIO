77586f09c5266704b22de3cb5c762ff511b59666
techno-cardi <tremblay.kevin@cscapitale.qc.ca>
2026-09-25 16:41:04 -0400

    Update AIO dashboard contract for Formative 1.1.14

--- Cardinal/AIO/aio-popup.test.js
+++ Cardinal/AIO/aio-popup.test.js
@@ -10,7 +10,7 @@ assert.deepEqual(
   Popup.MODULES.map(module => module.label),
   [
     'Gestion 1.1.9',
-    'Correction Formative 1.1.13',
+    'Correction Formative 1.1.14',
     'Importateur Formative 0.5 RC2',
     'Mozaïk v14',
     'Pont Classroom 1.2.3',
