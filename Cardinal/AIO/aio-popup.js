de1e47db1ac761b0074341023e2da4ad28ef4c29
techno-cardi <tremblay.kevin@cscapitale.qc.ca>
2026-09-25 16:38:31 -0400

    Label corrected AIO build as Formative 1.1.14

--- Cardinal/AIO/aio-popup.js
+++ Cardinal/AIO/aio-popup.js
@@ -19,7 +19,7 @@
 
   const MODULES = Object.freeze([
     Object.freeze({ id: 'gestion', label: 'Gestion 1.1.9' }),
-    Object.freeze({ id: 'formativeCorrection', label: 'Correction Formative 1.1.13' }),
+    Object.freeze({ id: 'formativeCorrection', label: 'Correction Formative 1.1.14' }),
     Object.freeze({ id: 'formativeImporter', label: 'Importateur Formative 0.5 RC2' }),
     Object.freeze({ id: 'mozaik', label: 'Mozaïk v14' }),
     Object.freeze({ id: 'classroom', label: 'Pont Classroom 1.2.3' }),
